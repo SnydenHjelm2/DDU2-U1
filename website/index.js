29264e3cef0d6938e82d4a2dd57900ddf74e6e64
Neo <neogoldahlgren@gmail.com>
2024-11-15 15:34:59 +0100

    Kodade funktionen createDistanceTable
    Kodade klart funktionen så att den skapar alla celler, och lägger rätt data i respektive cell. Även att rätt klass sätts på rätt cell så att tabellen ser ut som enligt facit.

--- website/index.js
+++ website/index.js
@@ -109,9 +109,60 @@ function getCityByName(cityName) {
     console.log(cityFound);
     return cityFound;
 }
+
+function createDistanceTable() {
+    for (i=-1; i<cities.length; i++) {
+        let cityCell = document.createElement("div");
+        
+        if (i === -1) {
+            cityCell.textContent = "";
+            cityCell.className += `cell`;
+            table.appendChild(cityCell);
+        } else {
+            if (cities[i].id % 2 === 0) {
+                cityCell.className += `even_row`;
+            }
+            cityCell.textContent = `${cities[i].id}-${cities[i].name}`;
+            cityCell.className += ` cell`;
+            cityCell.className += ` head_row`;
+            table.appendChild(cityCell);
+        }
+
+        for (j=0; j<cities.length; j++) {
+            let distanceCells = document.createElement("div");
+            distanceCells.className += `cell`;
+            if (i === -1) {
+                distanceCells.className += ` head_column`;
+                distanceCells.textContent = `${j}`;
+                table.appendChild(distanceCells);
+            } else {
+                if (i === j) {
+                    distanceCells.textContent = "";
+                    if (j % 2 === 0) {
+                        distanceCells.className += ` even_col`
+                    }
+                    table.appendChild(distanceCells);
+                } else {
+                    for (k=0; k<distances.length; k++) {
+                        if ((i === distances[k].city1 && j === distances[k].city2) || (i === distances[k].city2 && j === distances[k].city1)) {
+                            distanceCells.textContent = `${distances[k].distance / 10}`;
+                            if (j % 2 === 0) {
+                                distanceCells.className += ` even_col`
+                            }
+                            table.appendChild(distanceCells);
+                        }
+                    }
+                }
+            }
+        
+        
+        }
+    }
+}
 // Recommended: constants with references to existing HTML-elements
 const h2 = document.querySelector("h2");
 const citiesDiv = document.querySelector("#cities");
+const table = document.querySelector("#table");
 // Recommended: Ask for the city name and then the rest of the code
 //const userInput = prompt("City please!");
 //h2.textContent = userInput;
